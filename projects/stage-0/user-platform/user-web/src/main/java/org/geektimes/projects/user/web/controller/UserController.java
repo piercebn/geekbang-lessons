242c7a4fa02ac2dcfe196a7e955be5beba36bccb
baonan <baonan@chinahangaung.com>
2021-03-05 23:02:29 +0800

    add usercontroller

---
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/UserController.java
@@ -0,0 +1,26 @@
+package org.geektimes.projects.user.web.controller;
+
+import org.geektimes.web.mvc.controller.PageController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+@Path("/user")
+public class UserController implements PageController {
+
+    @GET
+    @POST
+    @Path("/register")
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+        if(request.getMethod().equals("GET")) {
+            return "register-form.jsp";
+        }else{
+
+            return "index.jsp";
+        }
+    }
+}
