78446b61fa0dcce02246e747b2250ef28a1f4fc6
baonan <piercebn@163.com>
2021-03-14 01:48:41 +0800

    通过Jolokia访问UserManager MXBean

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 极客时间课程工程
 
 work03
-1.UserManager改成MXBean，以支持自定义类型的展示
+
+1. UserManager改成MXBean，以支持自定义类型的展示
+2. 增加Jolokia支持http访问JMX UserManager MXBean，测试方式
+```
+//1.启动应用
+mvn tomcat7:run
+//2.验证http方式获取UserManager MXBean信息
+curl -X GET --header 'Accept: application/json' 'http://127.0.0.1:8080/user-web/jolokia/read/org.geektimes.projects.user.management:type=User’
+```
--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/TestingListener.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/listener/TestingListener.java
@@ -2,13 +2,16 @@ package org.geektimes.projects.user.web.listener;
 
 import org.geektimes.context.ComponentContext;
 import org.geektimes.projects.user.domain.User;
+import org.geektimes.projects.user.management.UserManager;
 import org.geektimes.projects.user.sql.DBConnectionManager;
 
+import javax.management.*;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.lang.management.ManagementFactory;
 import java.util.logging.Logger;
 
 /**
@@ -23,10 +26,11 @@ public class TestingListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
         ComponentContext context = ComponentContext.getInstance();
         DBConnectionManager dbConnectionManager = context.getComponent("bean/DBConnectionManager");
-        dbConnectionManager.getConnection();
+//        dbConnectionManager.getConnection();
         testPropertyFromServletContext(sce.getServletContext());
         testPropertyFromJNDI(context);
-        testUser(dbConnectionManager.getEntityManager());
+//        testUser(dbConnectionManager.getEntityManager());
+        testUserMXBean();
         logger.info("所有的 JNDI 组件名称：[");
         context.getComponentNames().forEach(logger::info);
         logger.info("]");
@@ -55,6 +59,26 @@ public class TestingListener implements ServletContextListener {
         entityManager.persist(user);
         transaction.commit();
         System.out.println(entityManager.find(User.class, user.getId()));
+    }
+
+    private void testUserMXBean() {
+        // 获取平台 MBean Server
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        // 为 UserMXBean 定义 ObjectName
+        ObjectName objectName = null;
+        try {
+            objectName = new ObjectName("org.geektimes.projects.user.management:type=User");
+            // 创建 UserMBean 实例
+            User user = new User();
+            user.setName("baonan");
+            user.setPassword("******");
+            user.setEmail("piercebn@163.com");
+            user.setPhoneNumber("1234567890");
+            mBeanServer.registerMBean(new UserManager(user), objectName);
+        } catch (Exception e) {
+            logger.severe("UserMXBean register with Exception : " + e.getMessage());
+        }
+        logger.info("UserMXBean register success");
     }
 
     @Override
