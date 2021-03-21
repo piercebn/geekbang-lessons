6c486f2583b00f9ea2b532dc55320bb4aaec64c6
baonan <piercebn@163.com>
2021-03-21 23:44:49 +0800

    get Config from HelloWorldController and read value

--- projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/HelloWorldController.java
+++ projects/stage-0/user-platform/user-web/src/main/java/org/geektimes/projects/user/web/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package org.geektimes.projects.user.web.controller;
 
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
 import org.geektimes.web.mvc.controller.PageController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -18,6 +20,16 @@ public class HelloWorldController implements PageController {
     @POST
     @Path("/world") // /hello/world -> HelloWorldController
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+        ConfigProviderResolver configProviderResolver = ConfigProviderResolver.instance();
+        Config config = configProviderResolver.getConfig(Thread.currentThread().getContextClassLoader());
+        //get config from DefaultResourceConfigSource
+        String appName = config.getValue("app.name", String.class);
+        Float appVersion = config.getValue("app.version", Float.class);
+        System.out.println("+++++ app name : " + appName + " , app version : " + appVersion);
+        //get config from JavaSystemProperitiesConfigSource
+        String awtToolkit = config.getValue("awt.toolkit", String.class);
+        Float javaVersion = config.getValue("java.specification.version", Float.class);
+        System.out.println("+++++ awtToolkit : " + awtToolkit + " , javaVersion : " + javaVersion);
         return "index.jsp";
     }
 }
