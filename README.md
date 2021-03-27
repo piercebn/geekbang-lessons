9a985c87906a4f7e1f6b103b1b5a342c8552a5fb
baonan <piercebn@163.com>
2021-03-27 22:59:31 +0800

    修复 org.geektimes.reactive.streams 包下的日志输出错误

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # geekbang-lessons
 极客时间课程工程
+
+## work05
+
+### 修复本程序 org.geektimes.reactive.streams 包下
+
+```
+问题：
+调整收到数据位置，先输出收到数据，在处理cancel逻辑
+
+验证：
+运行org.geektimes.reactive.streams.DefaultPublisher#main，日志输出
+收到数据：0
+收到数据：1
+收到数据：2
+本次数据发布已忽略，数据为：3
+本次数据发布已忽略，数据为：4
+
+```
+
+### 继续完善 my-rest-client POST 方法
+
+```
+
+```
+
+
--- projects/stage-0/user-platform/my-reactive-messaging/src/test/java/org/geektimes/reactive/streams/DefaultSubscriber.java
+++ projects/stage-0/user-platform/my-reactive-messaging/src/test/java/org/geektimes/reactive/streams/DefaultSubscriber.java
@@ -16,11 +16,11 @@ public class DefaultSubscriber<T> implements Subscriber<T> {
 
     @Override
     public void onNext(Object o) {
+        System.out.println("收到数据：" + o);
         if (++count > 2) { // 当到达数据阈值时，取消 Publisher 给当前 Subscriber 发送数据
             subscription.cancel();
             return;
         }
-        System.out.println("收到数据：" + o);
     }
 
     @Override
